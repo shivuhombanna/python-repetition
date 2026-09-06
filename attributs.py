f1b50a211bca291adf6ac7ecfab5d37c662a0acd
Shivaraj H <140573544+shivuhombanna@users.noreply.github.com>
2026-09-06 22:14:08 +0530

    Fix typo in Cars class and instances

--- attributs.py
+++ attributs.py
@@ -4,7 +4,7 @@ class Cars:
         self.module=module
     def cars_info(self):
         print(f"car brand name ={self.brand},car model name is {self.module}")
-bmw=Cars("BMW","M4 CS compitaion ")
+bmw=Cars("BMW","M4 CS compitaions ")
 bmw.cars_info()
 toyota=Cars("forchunar","4*4 drive")
 toyota.cars_info()
